6b7c06525b9777132a8eafbd74d00d611ed4bff8
Porkaew Jarusdamrongwat <por.porkaew15@gmail.com>
2020-11-17 13:31:38 +0700

    add lift component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'line-login-angular-v1';
+
+  ngOnInit(): void {
+    this.initLine();
+  }
+
+  initLine(): void {
+    liff.init({ liffId: '1655212012-qE4vgdJ5' }, () => {
+      if (liff.isLoggedIn()) {
+        this.runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  runApp(): void {
+    liff.getProfile().then(profile => {
+      console.log(profile);
+    }).catch(err => console.error(err));
+  }
 }
